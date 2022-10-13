48ef5da8adb85648c293f463c5ea27b4063f36c0
CoolfireOD <mishazinenkofighter@gmail.com>
2022-10-13 20:23:50 +0300

    feat: implement MUI

--- src/AddTodoItemInput/AddTodoItemInput.tsx
+++ src/AddTodoItemInput/AddTodoItemInput.tsx
@@ -1,5 +1,16 @@
 import React from "react";
 import "./AddTodoItemInput.css";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import { createTheme, ThemeProvider, styled } from "@mui/material/styles";
+import grey from "@mui/material/colors/grey";
+import { CustomButtonGray } from "../customElements";
+
+const theme = createTheme({
+  palette: {
+    info: grey,
+  },
+});
 
 interface AddTodoItemInputProps {
   onInputChange: (value: string) => void;
@@ -21,24 +32,32 @@ const AddTodoItemInput: React.FC<AddTodoItemInputProps> = ({
   }
 
   return (
-    <div className="add-todo-item">
-      <div className="add-todo-item__input-wrapper input-wrapper">
-        <input
-          type="text"
-          onKeyUp={(event: any) => {
-            if (event.key === "Enter") handleAddTodoItem();
-          }}
-          className="input-wrapper__input"
-          id="standard-basic"
-          value={value}
-          onChange={handleChange}
-        />
-      </div>
+    <ThemeProvider theme={theme}>
+      <div className="add-todo-item">
+        <div className="add-todo-item__input-wrapper input-wrapper">
+          <TextField
+            onKeyUp={(event) => {
+              if (event.key === "Enter") handleAddTodoItem();
+            }}
+            color="info"
+            className="input-wrapper__input"
+            id="standard-basic"
+            label="Task name"
+            variant="standard"
+            value={value}
+            onChange={handleChange}
+          />
+        </div>
 
-      <button className="add-todo-item__add-btn" onClick={handleAddTodoItem}>
-        add
-      </button>
-    </div>
+        <CustomButtonGray
+          variant="contained"
+          className="add-todo-item__add-btn"
+          onClick={handleAddTodoItem}
+        >
+          add
+        </CustomButtonGray>
+      </div>
+    </ThemeProvider>
   );
 };
 
