73b7b465301fb2508b2a2a8fbe0cd32a06588a03
mz_lam <63354875+CoolfireOD@users.noreply.github.com>
2022-11-23 20:56:17 +0200

    feat: implement creating new list (#7)
    * feat: implement creating new list
    
    * fix: change error message

--- src/routes/root/RootRoute.tsx
+++ src/routes/root/RootRoute.tsx
@@ -1,7 +1,35 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import { Box, Button, TextField } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { BASE_URL } from "../../const";
+import { TodoList } from "../../types";
+
+async function createList(value: string) {
+  if (value.trim() === "") throw new Error();
+
+  const list = {
+    name: value,
+  };
+
+  const response = await fetch(BASE_URL + `/lists`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(list),
+  });
+
+  return (await response.json()) as TodoList;
+}
 
 export const RootRoute: FC = () => {
+  const navigate = useNavigate();
+  const [value, setValue] = useState("");
+
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setValue(event.target.value);
+  }
+
   return (
     <Box
       sx={{
@@ -15,8 +43,16 @@ export const RootRoute: FC = () => {
         variant="standard"
         inputProps={{ maxLength: 64 }}
         fullWidth
+        onChange={handleChange}
       />
-      <Button variant="contained" type="submit">
+      <Button
+        variant="contained"
+        type="submit"
+        onClick={async () => {
+          const { id } = await createList(value);
+          navigate(`/lists/${id}`);
+        }}
+      >
         Create
       </Button>
     </Box>
